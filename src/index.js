db5c1897abcebc0580e1855dca3a17a44e719528
Vadim Glushkov <i@vadimhtml.ru>
2016-01-23 23:03:23 +0300

    v1 production

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+$(window).load(function () {
+    $('.bodyMain__tagLine')
+        .on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass('flip');
+        })
+        .click(function () {
+            $('.bodyMain__tagLine').addClass('flip');
+        })
+        .addClass('flip');
+});
